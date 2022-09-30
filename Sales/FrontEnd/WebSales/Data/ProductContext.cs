cfcff762ee7bd9365c51354e0afe96984809c5aa
Jhonatan Carranza Conde <jhonatanlifey@outlook.com>
2022-09-30 11:53:58 +0930

    Push Docker

---
+++ Sales/FrontEnd/WebSales/Data/ProductContext.cs
@@ -0,0 +1,37 @@
+﻿using WebSales.Models;
+
+namespace WebSales.Data
+{
+	public static class ProductContext
+	{
+		public static readonly List<Product> Products = new List<Product>
+		{
+
+			new Product()
+			{
+				Name = "I Phone X",
+				Description = "This phone is the company's biggest change to its flagship",
+				ImageFile = "product-1.png",
+				Price = 950.00M,
+				Category = "Smart Phopne"
+			},
+			new Product()
+			{
+				Name = "Samsung 10",
+				Description = "This phone is the company's biggest change to its flagship",
+				ImageFile = "product-2.png",
+				Price = 840.00M,
+				Category = "Smart Phopne"
+			},
+			new Product()
+			{
+				Name = "Xiomari",
+				Description = "This phone is the company's biggest change to its flagship",
+				ImageFile = "product-2.png",
+				Price = 740.00M,
+				Category = "Smart Phopne"
+			}
+
+		};
+	}
+}
